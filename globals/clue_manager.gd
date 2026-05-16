016a2b04266b117fee39daeb5cbf03d00b079d51
mysteriousTom <desertsoftworks@gmail.com>
2026-05-16 21:13:36 +0200

    cleanup in clue management

--- globals/clue_manager.gd
+++ globals/clue_manager.gd
@@ -1,12 +1,11 @@
 extends Node
 
 
-var all_clues: Dictionary[int,Clue] = {}
 var collected_clues: Array[Clue] = []
 
-func collect_clue(clue_id: int) -> void:
+func collect_clue(clue: Clue) -> void:
 	# When the player actually finds out about a clue, add it here
-	collected_clues.append(all_clues[clue_id])
+	collected_clues.append(clue)
 
 func clues_connected(clue1: Clue, clue2: Clue) -> bool:
 	# Returns if clues are related to each other using the clue properties
